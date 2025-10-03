84ea2fe8f41ec5796890aa5fdc70b2804c2dad65
Aditya Singh <adityasingh.cs1504@gmail.com>
2025-10-03 23:35:54 +0530

    Adding Matrix Operation code

--- 1 Python/6 Logging/a2_logging_application/logger.py
+++ 1 Python/6 Logging/a2_logging_application/logger.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(
     filename='application.log',
     filemode='w', # Can be w to overwrite and a to append
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s -%(name)s - %(levelname)s - %(message)s',# Format Log
     datefmt= '%Y-%m-%d %H:%M:%S',# setting Date format
     force=True
