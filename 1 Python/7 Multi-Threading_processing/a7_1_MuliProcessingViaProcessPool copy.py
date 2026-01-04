61b2562b1016535c89ae667294852da68ed409e3
Aditya Singh <adityasingh.cs1504@gmail.com>
2026-01-04 16:11:22 +0530

    EDA Codes

--- 1 Python/7 Multi-Threading_processing/a7_1_MuliProcessingViaProcessPool copy.py
+++ 1 Python/7 Multi-Threading_processing/a7_1_MuliProcessingViaProcessPool copy.py
@@ -11,7 +11,8 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 time1 = datetime.now()
 if __name__=="__main__":
     with ProcessPoolExecutor(max_workers=4) as ex:
-        futures = ex.map(print_number, numbers)
+        futures = ex.map(print_number, numbers) #here we use map so it returns a generator and we give list of numbers
+        #It is generator so even if task at poition 10 is completed first it will wait for previous tasks to complete
     print(type(futures)) # <class 'generator'> Calculated Value
     for result in futures:
         print(result)
